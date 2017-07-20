31641e9aa0e4ad792059cd06eac68ebfdd5b7b73
Nikolay Yakimov <root@livid.pp.ru>
2017-07-20 19:16:14 +0300

    Variables and rudimentary tests

--- parser_run.php
+++ parser_run.php
@@ -1,6 +1,20 @@
 <?php
 require('./parser.php');
 
+global $parser;
 $parser = new PhpPegJs\Parser;
+$parser->vars = array(
+    'test' => 3.14,
+);
 
-echo $parser->parse("1+3.1415e-1");
+function test($str, $expect) {
+    global $parser;
+    echo $str . ' => ' . $parser->parse($str) . ' ;; ' . $expect . "\n";
+}
+
+test('$test',3.14);
+test('if(in($test,3,4,5),1,0)',0);
+test('if(in($test,3.14,4,5),1,0)',1);
+test('if(in($test,3+0.14,sqrt(4),5),1,0)',1);
+test('if(in(2,3+0.14,sqrt(4),5),1,0)*sqrt(2)',sqrt(2));
+test('if(in(3,3+0.14,sqrt(4),5),1,0)*sqrt(2)',0);
